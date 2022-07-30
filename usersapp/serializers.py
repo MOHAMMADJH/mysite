3eaeb2c66d5bd749052fd474c1d54fca100c7037
mohjas <Mohjas97_fes@hotmail.com>
2022-07-30 20:58:17 +0300

    2

--- usersapp/serializers.py
+++ usersapp/serializers.py
@@ -14,3 +14,18 @@ class NoteSerializer(serializers.ModelSerializer):
     # def get_validation_exclusions(self):
     #         exclusions = super(note, self).get_validation_exclusions()
     #         return exclusions + ['owner']
+
+    """
+    
+    [
+    {
+        "note": "sadasd",
+        "email": "mohjas97_fes@hotmail.com",
+        "password": "1233456",
+        "self_d": "2022-07-23T17:27:06Z",
+        "note_name": "asd",
+        "note_id": "9613237a-c503-4e13-b279-84b5c18fe95f"
+    },
+    
+    
+    """
--- usersapp/views.py
+++ usersapp/views.py
@@ -14,12 +14,21 @@ class NotetList(APIView):
     """
     def get(self, request, format=None):
         Notes = note.objects.all()
+        print(' start Notes *********************************')
+        print('type :' + str(type(Notes)))
+        print(Notes.get(id=1))
+        print(' End Notes *********************************')
+
         serializer = NoteSerializer(Notes, many=True)
+        print('serializer :' + str(type(serializer)))
+        print('serializer :' + str(type(serializer.data)))
+
         return Response(serializer.data)
 
     def post(self, request, format=None):
         serializer = NoteSerializer(data=request.data)
         if serializer.is_valid():
+
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response({'name':'Response OK'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
